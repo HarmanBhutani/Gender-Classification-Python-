fec7d446982d7570a23b4243c667e3cb9a459525
Harman Bhutani <byteharman0101@gmail.com>
2017-07-25 00:08:11 +0530

    prediction completed

--- demo.py
+++ demo.py
@@ -2,10 +2,8 @@ from sklearn import tree
 
 clf = tree.DecisionTreeClassifier()
 
-# CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
+
+
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
@@ -15,12 +13,12 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
-
-# CHALLENGE - ...and train them on our data
 clf = clf.fit(X, Y)
+
+
 
 prediction = clf.predict([[155, 70, 43]])
 
-# CHALLENGE compare their reusults and print the best one!
+
 
 print(prediction)
